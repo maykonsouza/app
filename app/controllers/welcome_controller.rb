4dddb2853e1cbd455a504c96f6e69f46a5bf6fcd
Maykon Souza <maykon.souza@hotmail.com.br>
2020-12-16 00:54:36 -0300

    IniciandoAPP

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,26 @@
 class WelcomeController < ApplicationController
   def index
+
+    address = "www.facebook.com.br"
+    rtts = [10,24,75,12,27]
+    # for i in 1..5
+    #  result = `ping -c 3 #{address} | tail -1| awk '{print $4}' | cut -d '/' -f 2`.to_i
+    #  rtts.push(result)
+    #end
+    @latence2 = rtts.sum/rtts.length
+
+    if @latence2 < 30
+      indicator = ["success", "Ótima!"]
+    elsif @latence2 > 30 and @latence2 < 100
+        indicator = ["primary", "Boa"]
+    else
+      indicator = ["danger","Ruim!"]
+    end
+
+    @data2 = [{1 => rtts[0], 2 => rtts[1], 3 => rtts[2], 4 => rtts[3], 5 => rtts[4]}, address, @latence2, indicator ]
+
+
+
+
   end
 end
